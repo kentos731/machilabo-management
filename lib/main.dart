5ef84508debdb6a8ff7008f2fc9aafd1fe7d1d54
ケんとス <60173991+kentos731@users.noreply.github.com>
2023-04-06 17:35:47 +0900

    ミスしていたので

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,201 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+void main() => runApp(const MaterialApp(home: QRViewExample()));
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const QRViewExample(),
+            ));
+          },
+          child: const Text('qrView'),
+        ),
+      ),
+    );
+  }
+}
+
+class QRViewExample extends StatefulWidget {
+  const QRViewExample({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Expanded(flex: 3, child: _buildQrView(context)),
+            Expanded(
+              flex: 2,
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    if (result != null)
+                      Text(
+                          'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+                    else
+                      const Text('Scan a code'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.all(8),
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                await controller?.toggleFlash();
+                                setState(() {});
+                              },
+                              child: FutureBuilder(
+                                future: controller?.getFlashStatus(),
+                                builder: (context, snapshot) {
+                                  return Text('Flash: ${snapshot.data}');
+                                },
+                              )),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(8),
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                await controller?.flipCamera();
+                                setState(() {});
+                              },
+                              child: FutureBuilder(
+                                future: controller?.getCameraInfo(),
+                                builder: (context, snapshot) {
+                                  if (snapshot.data != null) {
+                                    return Text(
+                                        'Camera facing ${describeEnum(snapshot.data!)}');
+                                  } else {
+                                    return const Text('loading');
+                                  }
+                                },
+                              )),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.all(8),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.pauseCamera();
+                            },
+                            child: const Text('pause',
+                                style: TextStyle(fontSize: 20)),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(8),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.resumeCamera();
+                            },
+                            child: const Text('resume',
+                                style: TextStyle(fontSize: 20)),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
+
+
+
+/*import 'package:flutter/material.dart';
 import 'QRread.dart';
 
 void main() {
@@ -32,10 +229,55 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _admissionDialog() {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title :Text("来館ありがとうございます！"),
+        content: Text("Welcome to まちらぼ !"),
+      )
+    );
+  }
+  void _exitDialog() {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title :Text("ご利用ありがとうございました！"),
+        content: Text("See you !"),
+      )
+    );
+  }
+  void _firstDialog() {
+    showDialog(
+        context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text("まちラボをどこで知りましたか？"),
+          children: [
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context),
+              child: Text("webサイトやSNS"),
+            ),
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context),
+              child: Text("チラシやパンフレット"),
+            ),
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context),
+              child: Text("YouTubeなどの動画"),
+            ),
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context),
+              child: Text("たまたま立ち寄った"),
+            ),
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context),
+              child: Text("その他"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -58,70 +300,42 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ElevatedButton(
               onPressed: () => {
-                print('ボタンが押された！'),
-                _incrementCounter(),
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => QRread(title: 'QR read')),
+                  MaterialPageRoute(builder: (context) => QRread(title: "")),
                 ),
               },
               child: Text('QRコード読み取りのボタン'),
             ),
             ElevatedButton(
-              onPressed: () {_incrementCounter();},
-              child: Text('背景色を指定したボタン'),
+              onPressed: () {_admissionDialog();},
+              child: Text('入館ボタン'),
               style: ElevatedButton.styleFrom(
                 primary: Color.fromRGBO(255, 247, 153, 1), //ボタンの背景色
               ),
             ),
-            // * ボタンを押した時に色が変わる --------------------------------------
             ElevatedButton(
-              onPressed: () {_incrementCounter();},
-              child: Text(
-                '押した時赤になる',
-                style: TextStyle(color: Colors.white),
-              ),
+              onPressed: () {_exitDialog();},
+              child: Text('退館ボタン'),
               style: ElevatedButton.styleFrom(
-                primary: Colors.green, //ボタンの背景色
-                onPrimary: Colors.red, // ボタンを押した時の色
+                primary: Color.fromRGBO(255, 247, 153, 1), //ボタンの背景色
               ),
             ),
-            // * ボタンに枠線を付ける ----------------------------------------------
             ElevatedButton(
-              onPressed: () {_incrementCounter();},
-              child: Text('枠線付きボタン'),
+              onPressed: () {_firstDialog();},
+              child: Text('はじめての方'),
               style: ElevatedButton.styleFrom(
-                primary: Colors.purple, //ボタンの背景色
-                side: BorderSide(
-                  color: Colors.blue, // 枠線の色
-                  width: 5, // 枠線の太さ
-                ),
+                primary: Color.fromRGBO(255, 247, 153, 1), //ボタンの背景色
               ),
-            ),
-            // * .styleFromコンストラクタでサイズ変更 -----------------------------------------
-            ElevatedButton(
-              onPressed: () {_incrementCounter();},
-              child: Text('.styleFromコンストラクタでサイズ変更'),
-              style: ElevatedButton.styleFrom(
-                fixedSize: Size(250, 100), // サイズ変更
-                primary: Colors.pink,
-              ),
-            ),
-            // * .iconコンストラクタ ------------------------------------------------
-            // アイコン付きのボタンを作成できる
-            ElevatedButton.icon(
-              onPressed: () {_incrementCounter();},
-              icon: Icon(Icons.settings), // 表示するアイコン
-              label: Text('.iconコンストラクタ'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: (){},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), // This trailing comma makes auto-formatting nicer for build methods.*/
     );
   }
-}
+}*/
